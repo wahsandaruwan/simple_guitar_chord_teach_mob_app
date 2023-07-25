7270a73b8dfee465cc4a1c1b0921c3f1d97b3fcc
wahsandaruwan <wahsandaruwan6@gmail.com>
2023-07-25 15:09:26 +0530

    Created splash screen

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,9 +1,49 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  TouchableOpacity,
+  StatusBar,
+} from "react-native";
+import * as Icons from "react-native-heroicons/solid";
+
 export default function Splash() {
   return (
-    <View>
-      <Text>Splash</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar backgroundColor="orange" />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 20,
+          backgroundColor: "lightblue",
+        }}
+      >
+        <Icons.MusicalNoteIcon size="120" color="brown" />
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "bold",
+            marginVertical: 25,
+            color: "brown",
+          }}
+        >
+          Welcome to, Guitar Teacher!
+        </Text>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Home")}
+          style={{
+            backgroundColor: "brown",
+            paddingHorizontal: 20,
+            paddingVertical: 10,
+            borderRadius: 10,
+          }}
+        >
+          <Text style={{ color: "white" }}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
-const styles = StyleSheet.create({});
